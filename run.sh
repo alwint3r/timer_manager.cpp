86ab9652efde94514399b07536fb27d9f7d5cf87
Alwin Arrasyid <alwin.ridd@gmail.com>
2025-09-19 15:49:51 +0700

    Restructure the project directory.

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-set -e 
+set -e
 
-if [ ! -f "build/main" ]; then
-	echo "Build the project first by running ./build.sh"
-	exit 1
+BINARY="build/example_mac/example_mac"
+
+if [ ! -x "$BINARY" ]; then
+    echo "Build the project first by running ./build.sh"
+    exit 1
 fi
 
-./build/main "$@"
+"$BINARY" "$@"
